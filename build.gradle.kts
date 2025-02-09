3d98fca8a40667485203940a1e744e0fb4cfd83b
sanghohan@gmail.com <sanghohan@gmail.com>
2025-02-09 16:40:33 +0900

    redisson 테스트

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,10 +35,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
 	// redisson
-	implementation("org.redisson:redisson-spring-boot-starter:3.21.1")
+	implementation("org.redisson:redisson-spring-boot-starter:3.22.0")
 
 	// test
-	testImplementation("io.kotest:kotest-runner-junit5:4.4.3")
+	testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
 	testImplementation("io.kotest:kotest-assertions-core:4.4.3")
 	implementation("io.kotest:kotest-extensions-spring:4.4.3")
 
